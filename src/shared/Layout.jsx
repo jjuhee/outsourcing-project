be36dbe397c054fca75caebde379e54d6e440362
KANG0417 <ipia99@naver.com>
2023-12-05 17:35:45 +0900

    [future/basecode] 컴포넌트 폴더 이름 수정

--- src/shared/Layout.jsx
+++ src/shared/Layout.jsx
@@ -1,5 +1,5 @@
-import Footer from 'comments/Footer';
-import Header from 'comments/Header';
+import Footer from 'components/Footer';
+import Header from 'components/Header';
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 
