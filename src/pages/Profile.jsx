7995e3920b8204c92061efa97ffefe9665dd8c16
hc182 <khch503@naver.com>
2023-12-05 18:48:00 +0900

    heechan commit

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,7 +1,22 @@
 import React from "react";
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import styled from "styled-components";
 
 function Profile() {
-  return <div>Profile</div>;
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  return (
+    <Stwrapper>
+      <div> id : </div>
+      <div>내가 만든 코스</div>
+      <div>이미지</div>
+      <div>이미지</div>
+      <div>이미지</div>
+      <button>로그아웃</button>
+      <button onClick={() => navigate("/")}>홈으로</button>
+    </Stwrapper>
+  );
 }
 
 export default Profile;
