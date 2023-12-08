a87929dee626813717abb49fb18d0ddeaf5037fd
KANG0417 <ipia99@naver.com>
2023-12-09 02:38:21 +0900

    [feature/dating] 추천코스 컴포넌트 분리

--- src/components/mainPages/CompletedDatingCourse.jsx
+++ src/components/mainPages/CompletedDatingCourse.jsx
@@ -1,44 +1,42 @@
 import { getDatingCourses } from 'api/course';
-import { auth } from '../../firebase/firebase.config';
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { useQuery } from 'react-query';
-import { useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
 
 function CompletedDatingCourse() {
   const { isLoading, isError, data } = useQuery(['course'], getDatingCourses);
 
   return (
-    <StyledPlace>
-      <span>데이트 추천 코스</span>
+    <StPlaceWrapper>
+      <StRecommendTitle>데이트 추천 코스</StRecommendTitle>
       {data?.map((place) => {
         return (
-          <div style={{ border: '2px solid black' }}>
+          <StCourseWrapper style={{ border: '2px solid black' }}>
             {isLoading && <p>로딩중입니다!</p>}
             {isError && <p>서버오류 발생!</p>}
-            <p>코스명: {place.courseTitle}</p>
-            <p>작성날짜: {place.createAt}</p>
-            <StCourseWrapper>
+            <StCourseTitle>코스명: {place.courseTitle}</StCourseTitle>
+            <StWriteDay>작성날짜: {place.createAt}</StWriteDay>
+            <StCourseContainer>
               {place.place.map((item) => {
                 return (
-                  <StUl key={item.id}>
-                    <StLi>장소이름: {item.place_name}</StLi>
-                    <StLi>카테고리: {item.category_name}</StLi>
-                    <StLi>주소: {item.address_name}</StLi>
-                  </StUl>
+                  <StCourseList key={item.id}>
+                    <li>장소이름: {item.place_name}</li>
+                    <li>카테고리: {item.category_name}</li>
+                    <li>주소: {item.address_name}</li>
+                  </StCourseList>
                 );
               })}
-            </StCourseWrapper>
-          </div>
+            </StCourseContainer>
+          </StCourseWrapper>
         );
       })}
-    </StyledPlace>
+    </StPlaceWrapper>
   );
 }
 
 export default CompletedDatingCourse;
 
-const StyledPlace = styled.div`
+const StPlaceWrapper = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: center;
@@ -46,12 +44,32 @@ const StyledPlace = styled.div`
   & span {
     font-size: 40px;
   }
+  margin-top: 40px;
+`;
+
+const StRecommendTitle = styled.h2`
+  font-size: 1.5rem;
 `;
 
 const StCourseWrapper = styled.div``;
 
-const StUl = styled.ul`
-  border: 2px solid yellow;
+const StCourseTitle = styled.p`
+  font-size: 1.3rem;
 `;
 
-const StLi = styled.li``;
+const StWriteDay = styled.p``;
+
+const StCourseContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const StCourseList = styled.ul`
+  border: 2px solid black;
+  padding: 15px;
+
+  li {
+    font-size: 1rem;
+  }
+`;
