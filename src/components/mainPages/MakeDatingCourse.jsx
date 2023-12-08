9b3484eff1dbd7902bfedf4aca71a08e0fd1754c
jjuhee <azaza.hee@gmail.com>
2023-12-08 19:59:12 +0900

    [feature/courseQuery] 코스정보 리액트 쿼리로 구현

--- src/components/mainPages/MakeDatingCourse.jsx
+++ src/components/mainPages/MakeDatingCourse.jsx
@@ -1,38 +1,41 @@
 import React, { useState } from 'react';
-import { addDoc, collection, doc, setDoc } from 'firebase/firestore';
-import { db } from '../../firebase/firebase.config';
+import { auth } from '../../firebase/firebase.config';
 import uuid4 from 'uuid4';
 import dayjs from 'dayjs';
 import styled from 'styled-components';
+import { QueryClient, useMutation } from 'react-query';
+import { addDatingCourse } from 'api/course';
 
 function MakeDatingCourse({ selectedPlaces }) {
-  const courseApplyRef = collection(db, 'datingCourse');
   const [courseTitle, setCourseTitle] = useState('');
-  console.log('MakeDatingCourse', selectedPlaces);
-
+  const uuid = uuid4();
   const TODAY = dayjs().format('YY-MM-DD HH:mm:ss');
+  const user = auth.currentUser;
+  console.log(user);
+
+  const queryClient = new QueryClient();
+  const mutation = useMutation(addDatingCourse, {
+    onSuccess: () => {
+      // Invalidate and refresh
+      queryClient.invalidateQueries('course');
+    }
+  });
 
   const onClickCourseSaveButtonHandler = async (e) => {
     e.preventDefault();
-    const newDocRef = doc(courseApplyRef);
-
     if (courseTitle.trim() === '') {
       alert('제목을 입력해주세요!');
       return false;
     }
-    try {
-      await setDoc(newDocRef, {
-        courseUid: newDocRef.id,
-        // 현재 임시로 uuid 랜덤 생성 -> 유저 가입 uid로 바꿔놓기
-        userUid: uuid4(),
-        courseTitle: courseTitle,
-        place: selectedPlaces,
-        createAt: TODAY
-      });
-      alert('코스가 등록되었습니다!');
-    } catch (error) {
-      console.log(error);
-    }
+    mutation.mutate({
+      // 현재 임시로 uuid 랜덤 생성 -> 유저 가입 uid로 바꿔놓기
+      userUid: uuid,
+      courseTitle: courseTitle,
+      place: selectedPlaces,
+      createAt: TODAY
+    });
+
+    if (mutation.isSuccess) alert('코스가 등록되었습니다!');
   };
 
   const onTitleChange = (e) => {
