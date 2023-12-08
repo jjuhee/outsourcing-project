f32646b8d93f9ac5e9db5a807dc08033d641659d
chunmingyu <gyu4016@gmail.com>
2023-12-08 10:52:09 +0900

    [feature/login] 로그인 유효성 검사

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -3,8 +3,8 @@ import styled from 'styled-components';
 import { createUserWithEmailAndPassword } from 'firebase/auth';
 import { auth, db } from '../firebase/firebase.config';
 import { useNavigate } from 'react-router-dom';
-import { addDoc, collection } from 'firebase/firestore';
-import axios from 'axios';
+import { addDoc, collection, getDoc, getDocs, query, where } from 'firebase/firestore';
+
 function Signup() {
   const navigate = useNavigate();
 
@@ -14,28 +14,59 @@ function Signup() {
     nickname:'',
     avator:'',
   })
+  const {email, password, nickname, avator} = userInfo;
+
+
+  const changeUserInfoHandler = (event) => {
+    const { name, value } = event.target;
+    setUserInfo(prev=>({...prev, [name]:value}));
+  }
   
-  const inputEmailHandler = (e) => {
-    setUserInfo((prev) => ({...prev, email:e.target.value}))
-  }
+  const onSignupHandler = async (event)=> {
+    event.preventDefault();
 
-  const inputPasswordHandler = (e) => {
-    setUserInfo((prev) => ({...prev, password:e.target.value}))
-  }
+    //이메일 형식 @ or . 같은거 정규식으로 표현
+    const emailRegEx = /^[A-Za-z0-9]([-_.]?[A-Za-z0-9])*@[A-Za-z0-9]([-_.]?[A-Za-z0-9])*\.[A-Za-z]{2,3}$/;
+    const isEmailValid = emailRegEx.test(email);
+  
+    if(!isEmailValid) {
+      //이메일이 유효하지 않은 경우
+      alert('유효하지 않은 이메일 형식입니다.');
+      return;
+    }
+    
+    //비밀번호 글씨 길이 제한 유효성검사
+    const isPasswordValid = password.length >= 6 && password.length <= 15;
+  
+    if(!isPasswordValid) {
+      alert('비밀번호는 6자 이상, 15자 이하로 설정해야 합니다.');
+      return;
+    }
+  
+    //닉네임 글씨 제한 유효성검사
+    const isNicknameValid = nickname.length >= 1 && nickname.length <= 10;
+  
+    if (!isNicknameValid) {
+      alert('닉네임은 1자 이상, 10자 이하로 설정해야 합니다');
+      return;
+    }
 
-  const inputNicknameHandler = (e) => {
-    setUserInfo((prev) => ({...prev, nickname:e.target.value}))
-  }
+    //이메일 중복 확인
+    const emailQuery = query(collection(db, 'users'), where('email', '==', email));
+    const emailSnapshot = await getDocs(emailQuery);
 
-  const loginPageHandler = () => {
-    navigate('/login')
-  }
+    if (!emailSnapshot.empty) {
+      alert('이미 사용 중인 이메일입니다.');
+      return;
+    }
 
-  const signUp = async (e) => {
-    e.preventDefault();
+    //닉네임 중복 확인
+    const nicknameQuery = query(collection(db,'users'), where('nickname','==',nickname));
+    const nicknameSnapshot = await getDocs(nicknameQuery)
 
     try {
       const {email, password, nickname, avator } = userInfo
+
       // Firebase Authentication에 회원가입
       const userCredential = await createUserWithEmailAndPassword(
         auth,
@@ -51,27 +82,19 @@ function Signup() {
         avator,
       });
 
-      //사용할 일이 있나????
-      localStorage.setItem('accessToken', user.accessToken)
-      const accessToken = localStorage.getItem('accessToken')
-
       console.log('User added to Firestore with ID: ', userDocRef.id);
 
       // 회원가입 성공 시 메인 페이지로 이동
-      navigate('/');
+      alert('회원가입을 성공하셨습니다')
+      navigate('/login');
     } catch (error) {
       console.error(error);
     }
+  }
 
-    // try {
-    //   await axios.post('https://moneyfulpublicpolicy.co.kr/register', {
-    //     id,
-    //     nickname,
-    //     avator,
-    //     accessToken
-    //   })
-    // }
-  };
+  const loginPageHandler = () => {
+    navigate('/login')
+  }
 
   return (
     <>
@@ -79,23 +102,23 @@ function Signup() {
         <p>회원가입</p>
         <div>
           <label htmlFor='email'>Email</label>
-          <SignupInput id='email' type='email' value={userInfo.email}
-            onChange={inputEmailHandler}
+          <SignupInput type='email' id='email' name='email' value={userInfo.email}
+            onChange={changeUserInfoHandler}
             placeholder="이메일을 작성해주세요"
           />
           <label htmlFor='password'>Password</label>
-          <SignupInput id='password' value={userInfo.password}
-            onChange={inputPasswordHandler}
-            placeholder="암호를 작성해주세요(최소6자)"
+          <SignupInput type='password' id='password' name='password' value={userInfo.password}
+            onChange={changeUserInfoHandler}
+            placeholder="암호를 작성해주세요 (6~15글자)" minLength={6} maxLength={15}
           />
           <label htmlFor='nickname'>Nickname</label>
-          <ImgeInput id='nickname'
-            onChange={inputNicknameHandler}
-            placeholder="닉네임을 작성해주세요"
+          <ImgeInput type='text' id='nickname' name='nickname'
+            onChange={changeUserInfoHandler}
+            placeholder="닉네임을 작성해주세요 (1~10글자)" minLength={1} maxLength={10}
           />
         </div>
         <div>
-          <SignupButton onClick={signUp}>가입하기</SignupButton>
+          <SignupButton onClick={onSignupHandler}>가입하기</SignupButton>
           <SignupButton onClick={loginPageHandler}>로그인</SignupButton>
         </div>
       </form>
--- src/shared/Layout.jsx
+++ src/shared/Layout.jsx
@@ -1,34 +1,10 @@
-import axios from 'axios';
 import Footer from 'components/Footer';
 import Header from 'components/Header';
-import React, { useEffect, useState } from 'react';
-import { Outlet, useNavigate } from 'react-router-dom';
+import React from 'react';
+import { Outlet } from 'react-router-dom';
 
 function Layout() {
-  const navigate = useNavigate()
-  const [isRender, setIsRender] = useState(false)
 
-  useEffect(()=> {
-    const checkAuth = async () => {
-      const accessToken = localStorage.getItem('accessToken')
-      try {
-        const { data } = await axios.get('http://moneyfulpublicpolicy.co.kr/user', {
-          headers: {
-            'Content-Type':'application/json',
-            Authorization: `Bearer ${accessToken}`,
-            }
-        })
-        console.log('============================');
-        console.log(data);
-        console.log('============================');
-      } catch(error) {
-        alert('알 수 없는 오류가 발생하였습니다.')
-        localStorage.removeItem('accessToken')
-        navigate('/login')
-      }
-    };
-    checkAuth();
-  },[])
   return (
     <>
       <Header />
