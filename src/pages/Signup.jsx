c2ba8f1d87a4f9fa14c1d4c8aedc1c8a2ad10116
HONGSANGYOON <jay182@naver.com>
2023-12-08 11:34:15 +0900

    [feature/login]회원가입시 이미지 업로드 및 db저장 완료

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,20 +1,17 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
-import { createUserWithEmailAndPassword } from 'firebase/auth';
-import { auth, db } from '../firebase/firebase.config';
+import { createUserWithEmailAndPassword, updateProfile } from 'firebase/auth';
+import { auth, db, storage } from '../firebase/firebase.config';
 import { useNavigate } from 'react-router-dom';
 import { addDoc, collection } from 'firebase/firestore';
-import { onAuthStateChanged } from 'firebase/auth';
-import {
-  createStorageRef,
-  getDownloadFileURL,
-  getDefaultProfileImgURL
-} from 'firebase/storage';
+import { ref, uploadBytes, getDownloadURL } from 'firebase/storage';
+
 function Signup() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [nickname, setNickname] = useState('');
   const [selectedFile, setSelectedFile] = useState(null);
+  const [previewURL, setPreviewURL] = useState(null);
 
   const navigate = useNavigate();
 
@@ -31,11 +28,29 @@ function Signup() {
 
       // Firebase Firestore에 사용자 정보 추가
       const user = userCredential.user;
+      let downloadURL;
+      // 이미지를 Storage에 업로드
+      if (selectedFile) {
+        const imageRef = ref(
+          storage,
+          `profileImages/${user.uid}/${selectedFile.name}`
+        );
+        console.log(imageRef);
+        await uploadBytes(imageRef, selectedFile);
+        downloadURL = await getDownloadURL(imageRef);
+        console.log({ downloadURL });
+        // 사용자 정보에 프로필 이미지 URL 추가
+        await updateProfile(user, {
+          photoURL: downloadURL
+        });
+      }
+
+      // Firestore에 사용자 정보 추가
       const userDocRef = await addDoc(collection(db, 'users'), {
         uid: user.uid,
         email: user.email,
         nickname,
-        avator: selectedFile
+        avator: downloadURL
       });
 
       console.log('User added to Firestore with ID: ', userDocRef.id);
@@ -48,61 +63,48 @@ function Signup() {
   };
 
   const handleFileSelect = (event) => {
-    setSelectedFile(event.target.files[0]);
+    const file = event.target.files[0];
+
+    // 선택한 파일의 미리보기를 생성
+    if (file) {
+      const reader = new FileReader();
+      reader.onload = (e) => {
+        setPreviewURL(e.target.result);
+      };
+      reader.readAsDataURL(file);
+    }
+
+    setSelectedFile(file);
   };
-
-  const handleUpload = async () => {
-    // ref 함수를 이용해서 Storage 내부 저장할 위치를 지정하고, uploadBytes 함수를 이용해서 파일을 저장합니다.
-    const imageRef = ref(
-      storage,
-      `${auth.currentUser.uid}/${selectedFile.name}`
-    );
-    await uploadBytes(imageRef, selectedFile);
-
-    // 파일 URL 가져오기
-    const downloadURL = await getDownloadURL(imageRef);
-    console.log(downloadURL);
-  };
-
-  useEffect(() => {
-    onAuthStateChanged(auth, (user) => {
-      console.log('user', user); // 사용자 인증 정보가 변경될 때마다 해당 이벤트를 받아 처리합니다.
-    });
-  }, []);
 
   return (
     <>
       <form>
         <div>
           <SignupInput
-            onChange={(e) => {
-              setEmail(e.target.value);
-            }}
+            onChange={(e) => setEmail(e.target.value)}
             placeholder="아이디를 작성해주세요"
           />
           <SignupInput
-            onChange={(e) => {
-              setPassword(e.target.value);
-            }}
+            onChange={(e) => setPassword(e.target.value)}
             placeholder="암호를 작성해주세요(최소6자)"
           />
           <SignupInput
-            onChange={(e) => {
-              setNickname(e.target.value);
-            }}
+            onChange={(e) => setNickname(e.target.value)}
             placeholder="닉네임을 작성해주세요"
           />
-          <signupProfileImg
-            placeholder="닉네임을 작성해주세요"
-            htmlFor="profileImg"
-          />
+          <SignupProfileImg htmlFor="profileImg">
+            프로필 이미지 업로드
+          </SignupProfileImg>
+          {previewURL && (
+            <ImagePreview src={previewURL} alt="프로필 이미지 미리보기" />
+          )}
           <ImgeInput
-            Type="file"
-            accept="image/"
+            type="file"
+            accept="image/*"
             id="profileImg"
             onChange={handleFileSelect}
           />
-          <button onClick={handleUpload}>업로드하기</button>
         </div>
         <div>
           <SignupButton onClick={signUp}>가입하기</SignupButton>
@@ -117,27 +119,30 @@ export default Signup;
 const SignupInput = styled.input`
   background-color: #ffe7cf;
   width: 100%;
-  /* outline: none; */
-  /* border: none;
-  border-radius: 0.5rem; */
 `;
+
 const SignupButton = styled.button`
-  /* background-color: gray; */
   width: 10vw;
   height: 5vh;
   border-radius: 2px;
 `;
+
 const ImgeInput = styled.input`
   background-color: #ffe7cf;
   width: 100%;
-  /* display: none; */
+  display: none;
 `;
 
-const signupProfileImg = styled.label`
+const SignupProfileImg = styled.label`
   margin: 5px 0 20px 0;
   font-weight: bold;
   font-size: 13px;
   color: #0095f6;
   display: inline-block;
   cursor: pointer;
+`;
+
+const ImagePreview = styled.img`
+  width: 100%;
+  max-height: 200px;
 `;
